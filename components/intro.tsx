0c4292fb23a08e762534b512d8681e5acaf95afa
JEPHTHAH Mbah <jephthah.mbah@outlook.com>
2023-08-23 23:53:31 +0100

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -64,9 +64,9 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Jephthah.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">3 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">MERN STACK and (Next.js)</span>.
+        <span className="font-bold">3 years</span> of experience. I find immense
+        joy in crafting websites and Mobile applications. My focus is{" "}
+        <span className="underline">MERN STACK and (Next.js)</span>
       </motion.h1>
 
       <motion.div
